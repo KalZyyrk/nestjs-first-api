70535af537be3f10b0109427e7acaac745afbd93
Ulysse Hischke <u.hischke@outlook.fr>
2023-08-10 18:27:16 +0200

    add route for favorite book

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('book')
-export class BookController {}
+export class BookController {
+  @Get()
+  findAll(): string {
+    return 'book';
+  }
+
+  @Get('favorites')
+  findAllFavorites(): string {
+    return 'favorites book';
+  }
+}
